b906b58dac73aff8d0936eed47b9e946721f8517
aishikmaitra-2003 <aishikmaitra2003@gmail.com>
2025-12-03 01:39:10 +0530

    Logger added

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,7 @@
+import logging
+import os
+from datetime import datetime
+LOG_FILE=f"{datetime.now().strftime('%d_%m_%Y_%H')}.log"
+logs_path=os.path.join(os.getcwd(),"Logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
